2dfc4d101a2e70a305bce86088a31f024728a891
Macy Huang <mjmj3794@gmail.com>
2024-09-04 00:24:55 +0800

    lv1

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 import './assets/scss/all.scss';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
 
-console.log("Hello world!");
+console.log('Hello world!');
